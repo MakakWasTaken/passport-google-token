0d28c5937078fa2342fbde0a7c4fc1ee73152306
Markus Moltke <makakwastaken@gmail.com>
2021-06-11 01:31:42 +0200

    Updated description in type file

--- lib/passport-google-token/index.d.ts
+++ lib/passport-google-token/index.d.ts
@@ -1,6 +1,6 @@
-// Type definitions for passport-facebook-token 0.4
-// Project: https://github.com/drudge/passport-facebook-token
-// Definitions by: Ray Martone <https://github.com/rmartone>, Michael Randolph <https://github.com/mrand01>
+// Type definitions for passport-google-token 0.4
+// Project: https://github.com/drudge/passport-google-token
+// Definitions by: Markus Moltke <https://github.com/makakwastaken>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
